73d9739601f2a852e25aa8465709bde7651f4b73
Hu Tao <ariunboldbold200@gmail.com>
2026-04-11 23:50:57 +0800

    yea were gonna bounce from the railway since its expensiveasf we will be using vercel from now on

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-require("dotenv").config();
-const express = require("express");
-const { connect, getBucket } = require("./mongo_db/mongodb");
-const uploadRoutes = require("./mongo_db/upload");
-const cors = require("cors");
-
+import express from "express";
+import { connect } from "./mongo_db/mongodb.js";
+import router from "./mongo_db/upload.js";
+import cors from "cors";
+import dotenv from "dotenv";
 const app = express();
-
+dotenv.config();
 // Connect to MongoDB and GridFS
 connect();
 
@@ -15,10 +14,11 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use("/api", uploadRoutes);
+app.use("/api", router);
 
-app.get("/",(req,res)=>{ res.status(200) ;res.send("psdaoaoaoaoo")})
-
-app.listen(8080, () => {
-  console.log(`Server is running on port`);
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("psdaoaoaoaoo");
 });
+
+export default app;
